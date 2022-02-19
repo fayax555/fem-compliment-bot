257c9fb675350d5ca944b23d3d122997a9c02798
fayax555 <42441836+fayax555@users.noreply.github.com>
2022-02-19 21:54:42 +0500

    add puppeteer automation

--- index.js
+++ index.js
@@ -1,5 +1,71 @@
-import fetch from 'node-fetch'
-import { chromium } from 'playwright'
+import 'dotenv/config'
+import puppeteer from 'puppeteer-extra'
+import StealthPlugin from 'puppeteer-extra-plugin-stealth'
+import { getRandom } from 'random-useragent'
+import { sleep } from './utils'
+
+const launchPuppeteer = async () => {
+  puppeteer.use(StealthPlugin())
+
+  const USER_AGENT =
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.75 Safari/537.36'
+
+  const browser = await puppeteer.launch({
+    headless: false,
+    executablePath: process.env.CHROME_BIN || null,
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      // `--proxy-server='${newProxyUrl}`,
+    ],
+    ignoreHTTPSErrors: true,
+    dumpio: false,
+  })
+  const page = await browser.newPage({ headless: false })
+  const userAgent = getRandom()
+  const UA = userAgent || USER_AGENT
+
+  //Randomize viewport size
+  // await page.setViewport({
+  //   width: 1920 + Math.floor(Math.random() * 100),
+  //   height: 3000 + Math.floor(Math.random() * 100),
+  //   deviceScaleFactor: 1,
+  //   hasTouch: false,
+  //   isLandscape: false,
+  //   isMobile: false,
+  // })
+
+  await page.setUserAgent(UA)
+  await page.setJavaScriptEnabled(true)
+  page.setDefaultNavigationTimeout(0)
+  await page.goto('https://www.frontendmentor.io/', {
+    waitUntil: 'networkidle0',
+  })
+
+  return { page, browser }
+}
+
+const auth = async () => {
+  const { page, browser } = await launchPuppeteer()
+
+  await Promise.all([
+    page.waitForNavigation(),
+    page.click(
+      'html.js body div#__next div#content main section.Home__Intro-sc-irpn59-0.iROMog.intro div.container div.Home__Content-sc-irpn59-1.eTfqHg span.GithubAuthButton__Wrapper-sc-1t3homg-0.iTwlcw.gh-auth a.GithubAuthButton__Link-sc-1t3homg-1.edcjIF'
+    ),
+  ])
+  await page.type('#login_field', process.env.EMAIL, { delay: 100 })
+  await page.type('#password', process.env.PASSWORD, { delay: 100 })
+  await page.click(
+    '#login > div.auth-form-body.mt-3 > form > div > input.btn.btn-primary.btn-block.js-sign-in-button'
+  )
+
+  await page.waitForSelector(
+    '#__next > header > div > div > button > div.Avatar__Wrapper-sc-1t4kc72-0.jNTFqT'
+  )
+
+  return { page, browser }
+}
 
 const getData = async () => {
   const { data } = await (
@@ -15,26 +81,20 @@ const getData = async () => {
 }
 
 ;(async function main() {
-  try {
-    const browser = await chromium.launch({
-      headless: false,
-      args: ['--no-sandbox', '--disable-setuid-sandbox'],
-      proxy: {
-        server: '187.19.152.182:3128',
-      },
-    })
+  const { browser, page } = await auth()
 
-    const page = await browser.newPage()
+  await page.goto(
+    'https://www.frontendmentor.io/solutions/rest-country-finder-y8glEosLZ'
+  )
 
-    await page.goto(
-      'https://www.frontendmentor.io/solutions/rest-country-finder-y8glEosLZ'
-    )
-  } catch (err) {
-    console.error(err)
-  }
+  const [el] = await page.$x(
+    '/html/body/div[1]/div[3]/div[2]/div[2]/section/div/form/div/div/textarea'
+  )
+  await el.type('great job!')
 
-  // const data = await getData()
-  // console.log(data)
+  console.log('typed')
+
+  // await page.type('#feedback form textarea', 'great job', { delay: 100 })
 
   // await browser.close()
 })()
